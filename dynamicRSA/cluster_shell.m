ba0e9952ca19bf32ded23e7be305bf7288c857c5
Ingmar de Vries <112584671+Ingmar-de-Vries@users.noreply.github.com>
2022-09-05 16:29:42 +0200

    Add files via upload

---
+++ dynamicRSA/cluster_shell.m
@@ -0,0 +1,27 @@
+function cluster_shell(cfg,script2run)
+% determine whether to send job to cluster, or run script locally
+
+if isfield(cfg,'cluster')% send to cluster
+    
+    matlab_version = sscanf(version('-release'),'%d%s');
+    if matlab_version(1) ~= 2020
+        error('The package for sending jobs to the cluster only works on matlab 2020, please use that');
+    end
+
+    cluster = parcluster;
+    job = createJob(cluster);
+    
+    % Assign tasks using your function
+    for iroi=cfg.ROIVec
+        for isub = cfg.SubVec
+            createTask(job,str2func(script2run), 0,{cfg, isub, iroi});%
+        end
+    end
+    % run task
+    submit(job);
+    
+else% run locally
+    
+    feval(script2run,cfg,0,0);%if run locally, looping over subjects and ROIs happens within function (saves time as you only have to load the data of a subject once for all ROIs)
+    
+end
